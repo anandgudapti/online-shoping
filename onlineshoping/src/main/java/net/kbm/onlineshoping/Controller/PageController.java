18c3caaab17c5c72c9c60e4823833e8f7af2ea07
anandgudapati <anand.gudapti10@gmail.com>
2017-07-10 23:48:01 +0530

    spring connection done

--- onlineshoping/src/main/java/net/kbm/onlineshoping/Controller/PageController.java
+++ onlineshoping/src/main/java/net/kbm/onlineshoping/Controller/PageController.java
@@ -2,18 +2,25 @@ package net.kbm.onlineshoping.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController 
 {
-	@RequestMapping(value="/")
+	@RequestMapping(value={"/","index","home"})
 	public ModelAndView index()
 	{
 		ModelAndView mv=new ModelAndView("page");
 		mv.addObject("messgae", "message");
 		return mv;
-		
+	}
+	@RequestMapping("/test")
+	public ModelAndView nextp(@RequestParam("greeting")String greeting)
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
 	}
 
 }
